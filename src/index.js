7e20813b5e2d648ed4c8feb225f96d9d79f1454a
PaynePew <42099584+PaynePew@users.noreply.github.com>
2021-07-26 03:51:05 +0800

    section 2 ending
    JSX vs HTML practice

---
+++ src/index.js
@@ -0,0 +1,40 @@
+// Import the React and ReactDOM libraries
+import { directive } from '@babel/types';
+import React from 'react';
+import ReactDOM from 'react-dom'
+// Live Reloading
+if (module.hot) {
+    module.hot.accept();
+  }
+
+function getButtonText() {
+    return 'Click on Me!'
+}
+
+// create a react component
+
+
+const App = () => {
+    const buttonText = { text: 'Click me' }
+    const style = {backgroundColor:'blue', color:'white'}
+    const labelText = 'Enter name:'
+
+    return ( 
+    <div>
+    <label className="label" htmlFor="name">
+      {labelText}
+    </label>
+    <input type="text" id="name" />
+    <button style={style}>
+        {buttonText.text}
+    </button>
+    {/* <button style="background-color:blue; color: white">Submit</button> */}
+  </div>
+  )
+}
+
+// Take the react component and show it on the screen
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+)
