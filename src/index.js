a9b5520a8dcab6ac2280a3b03d114a97056055aa
PaynePew <42099584+PaynePew@users.noreply.github.com>
2021-07-27 10:37:12 +0800

    style: javascript standard style test

--- src/index.js
+++ src/index.js
@@ -1,40 +1,39 @@
 // Import the React and ReactDOM libraries
-import { directive } from '@babel/types';
-import React from 'react';
+import { directive } from '@babel/types'
+import React from 'react'
 import ReactDOM from 'react-dom'
 // Live Reloading
 if (module.hot) {
-    module.hot.accept();
-  }
+  module.hot.accept()
+}
 
-function getButtonText() {
-    return 'Click on Me!'
+function getButtonText () {
+  return 'Click on Me!'
 }
 
 // create a react component
 
-
 const App = () => {
-    const buttonText = { text: 'Click me' }
-    const style = {backgroundColor:'blue', color:'white'}
-    const labelText = 'Enter name:'
+  const buttonText = { text: 'Click me' }
+  const style = { backgroundColor: 'blue', color: 'white' }
+  const labelText = 'Enter name:'
 
-    return ( 
+  return (
     <div>
-    <label className="label" htmlFor="name">
-      {labelText}
-    </label>
-    <input type="text" id="name" />
-    <button style={style}>
+      <label className='label' htmlFor='name'>
+        {labelText}
+      </label>
+      <input type='text' id='name' />
+      <button style={style}>
         {buttonText.text}
-    </button>
-    {/* <button style="background-color:blue; color: white">Submit</button> */}
-  </div>
+      </button>
+      {/* <button style="background-color:blue; color: white">Submit</button> */}
+    </div>
   )
 }
 
 // Take the react component and show it on the screen
 ReactDOM.render(
-    <App />,
-    document.querySelector('#root')
+  <App />,
+  document.querySelector('#root')
 )
